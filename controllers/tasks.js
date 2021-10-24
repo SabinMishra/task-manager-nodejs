54fe15f58b2a5dabc5ffa08fa73eea9b41f27cb8
Sabin Mishra <predictevsabin@gmail.com>
2021-10-24 22:35:39 +0545

    PRoject completed

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,8 +1,13 @@
 const Task = require('../models/tasks')
 
 const getAllTasks = async (req,res)=>{
-    
-    res.send('all items from the file')
+    try{
+        const tasks = await Task.find({})
+        res.status(200).json({tasks})
+    }catch(error){
+        res.status(500).json({msg:error})
+    }
+   
 }
 
 const createTask = async (req,res)=>{
@@ -16,16 +21,54 @@ const createTask = async (req,res)=>{
     
 }
 
-const getTask = (req,res)=>{
-    res.json({id:req.params.id})
+const getTask = async (req,res)=>{
+    try{
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID})
+       
+        if(!task){
+            return res.status(404).json({msg:`No task with this id ${taskID}`})
+        }
+        res.status(200).json({task})
+    }catch(error){
+        res.status(500).json({msg:error})
+    }
 }
 
-const updateTask = (req,res)=>{
-    res.send('update task')
+
+//---------------------------------------------UPDATE TASK----------------------------------------------------------------
+const updateTask = async (req,res)=>{
+    try{
+        const {id:taskID} = req.params;
+        const task = await Task.findOneAndUpdate({_id:taskID},req.body,{new:true,runValidators:true})
+        if(!task){
+            res.status(404).json({msg:`No task with the id: ${taskID}`})
+        }
+
+        res.json({task}).status(200)
+    }catch(error){
+        res.status(500).json({msg:error})
+    }
 }
 
-const deleteTask = (req,res)=>{
-    res.send('delete task ')
+
+
+
+//---------------------------------------------DELETE TASK----------------------------------------------------------------
+const deleteTask =async (req,res)=>{
+   try{
+    const {id:taskID} = req.params
+    const task = await Task.findOneAndDelete({_id:taskID})
+    if(task){
+        res.status(200).json({task})
+    }
+    else if(!task){
+        res.status(404).json({msg:`No task with id:${taskID}`})
+    }
+   }
+   catch(error){
+    res.status(500).json({msg:error})
+   }
 }
 
 
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 
 
-const PORT = 3000;
+const PORT =process.env.PORT || 3000;
 
 
 //middleware
@@ -21,12 +21,6 @@ app.use(express.json())
 app.use('/api/v1/tasks',tasks);
 
 
-
-//app.get('/api/v1/tasks)
-//app.post('/api/v1/tasks)
-//app.get('/api/v1/tasks/:id)
-//app.patch('/api/v1/tasks/:id)
-//app.delete('/api/v1/tasks/:id)
 const start = async()=>{
     try{
         await connectDB(process.env.MONGO_URI)
